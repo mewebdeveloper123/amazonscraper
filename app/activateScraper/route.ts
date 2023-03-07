57c8653d71239a709c57e8de9c81fe85f7e5867e
mewebdeveloper123 <mewebdeveloper123@gmail.com>
2023-03-07 16:36:24 +0530

    added route

---
+++ app/activateScraper/route.ts
@@ -0,0 +1,10 @@
+export async function POST(req:Request) {
+    
+}
+
+export async function GET(req:Request) {
+    return new Response("Hello from New API", {
+        status: 200,
+    })
+    
+}
